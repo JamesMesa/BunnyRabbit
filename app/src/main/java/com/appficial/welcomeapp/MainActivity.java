bb7931287ba4d49d51b563f4df19816259ac182a
James <jgappy@sdccd.edu>
2017-10-27 11:26:25 -0700

    added a comment

--- app/src/main/java/com/appficial/welcomeapp/MainActivity.java
+++ app/src/main/java/com/appficial/welcomeapp/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //set the content view to the activity_main.xml file
+        // that is where the bunny rabbit image is located
         setContentView(R.layout.activity_main);
     }
 }
